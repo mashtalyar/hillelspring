d3bd02cf910387c68021f9df57ae8bfc54901e9b
mashtalyar <111196251+mashtalyar@users.noreply.github.com>
2023-01-30 19:01:10 +0200

    Add files via upload
    push hw32

---
+++ hillelspring/src/main/java/ua/ithillel/hillelspring/HillelspringApplication.java
@@ -0,0 +1,30 @@
+package ua.ithillel.hillelspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import ua.ithillel.hillelspring.controller.PhoneController;
+import ua.ithillel.hillelspring.entity.Phone;
+
+import java.util.List;
+
+@SpringBootApplication
+public class HillelspringApplication {
+    public static void main(String[] args) {
+//        SpringApplication.run(HillelspringApplication.class, args);
+//        final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(BeanConfiguration.class);
+        final ConfigurableApplicationContext context = SpringApplication.run(HillelspringApplication.class, args);
+        final PhoneController phoneController = context.getBean(PhoneController.class);
+        final List<Phone> all = phoneController.getAll();
+        all.forEach(System.out::println);
+//        final String stringBean = context.getBean("stringBean", String.class);
+//        final Phone customPhone = context.getBean("getCustomPhone", Phone.class);
+//        System.out.println(stringBean);
+//        System.out.println(customPhone);
+//
+//        final String botName = context.getBean("getBotName", String.class);
+//        final int botHp = context.getBean("getBotHP", int.class);
+//        System.out.println("Bot name:"+botName);
+//        System.out.println("Bot hp:"+botHp);
+    }
+}
---
+++ hillelspring/src/main/java/ua/ithillel/hillelspring/controller/PhoneController.java
@@ -0,0 +1,22 @@
+package ua.ithillel.hillelspring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import ua.ithillel.hillelspring.entity.Phone;
+import ua.ithillel.hillelspring.service.PhoneService;
+
+import java.util.List;
+
+@Controller
+public class PhoneController {
+    private final PhoneService phoneService;
+
+    @Autowired
+    public PhoneController(PhoneService phoneService) {
+        this.phoneService = phoneService;
+    }
+
+    public List<Phone> getAll() {
+        return phoneService.getAll();
+    }
+}
